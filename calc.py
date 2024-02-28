6f2c9bcf9e973b2f68895573052a1de0714dd94d
Dormuma14 <gioshalamberidze83@gmail.com>
2024-02-28 14:28:46 +0400

    new mod calc.py 14:28

--- calc.py
+++ calc.py
@@ -2,3 +2,9 @@ x = int(input("X: "))
 y = int(input("Y: "))
 
 print(f"Sum: {x + y}")
+print(f"Sum: {x - y}")
+print(f"Sum: {x * y}")
+if y == 0:
+	print("Can't do it!")
+else:
+	print(f"Div: {x / y}")
